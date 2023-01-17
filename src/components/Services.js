c9882b057b9e9c60650303e894f2fdbb77811f1c
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-01-17 11:39:08 +0200

    added

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,20 +1,98 @@
 import React from 'react';
 // img
 import Img from '../assets/image.png';
+// icon
+import { BsArrowUpRight } from 'react-icons/bs';
+// motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+// services
+const services = [
+  {
+    name: 'UI/UX DESIGN',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac metus ac erat iaculis pretium. Nullam vel nibh pulvinar.',
+    link: 'Learn more',
+  },
+  {
+    name: 'DEVELOPMENT',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac metus ac erat iaculis pretium. Nullam vel nibh pulvinar.',
+    link: 'Learn more',
+  },
+  {
+    name: 'DIGITAL MARKETING',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac metus ac erat iaculis pretium. Nullam vel nibh pulvinar.',
+    link: 'Learn more',
+  },
+  {
+    name: 'PRODUCT BRANDING',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac metus ac erat iaculis pretium. Nullam vel nibh pulvinar.',
+    link: 'Learn more',
+  },
+];
+
 const Services = () => {
   return (
     <section id='services' className='section'>
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row'>
-          <div className='flex-1'>
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.7 }}
+            className='flex-1'
+          >
             <h2 className='h2 text-accent mb-6'>What I Do.</h2>
-            <h3 className='h3 mb-12'>
+            <h3 className='h3 mb-12 max-w-[455px]'>
               I'm a Freelancer Front-end Developer with over 5 years of
               experience.
             </h3>
             <img src={Img} alt='' />
-          </div>
-          <div className='flex-1'>services</div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn('left', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.7 }}
+            className='flex-1'
+          >
+            {/* service list */}
+            <div>
+              {services.map((service, index) => {
+                const { name, description, link } = service;
+                return (
+                  <div
+                    className='border-b border-white/20 h-[146px] mb-[38px] flex'
+                    key={index}
+                  >
+                    <div className='max-w-[476px]'>
+                      <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6'>
+                        {name}
+                      </h4>
+                      <p className='font-secondary leading-tight'>
+                        {description}
+                      </p>
+                    </div>
+                    <div className='flex flex-1 flex-col items-end'>
+                      <a
+                        href='#'
+                        className='btn w-9 h-9 rounded-full mb-[42px] flex justify-center items-center'
+                      >
+                        <BsArrowUpRight />
+                      </a>
+                      <a className='text-gradient text-sm' href='#'>
+                        {link}
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
