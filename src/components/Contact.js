c6ece2c067767e047f201ff8962ef2b63f8d2361
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-01-21 13:53:59 +0200

    added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ import { fadeIn } from '../variants';
 
 const Contact = () => {
   return (
-    <section className='section' id='contact'>
+    <section className='py-16 lg:section' id='contact'>
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row'>
           <motion.div
